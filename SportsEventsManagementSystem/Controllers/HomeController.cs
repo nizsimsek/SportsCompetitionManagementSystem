76871ac39f38a05701bea82bc87d2f5572be268e
nizsimsek <19720111036@ybu.edu.tr>
2021-06-25 16:36:26 +0300

    Add project files.

---
+++ SportsEventsManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsEventsManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Giriş Yap";
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.RemoveAll();
+            return View(Login());
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Anasayfa";
+
+            return View();
+        }
+        public ActionResult PlayerList()
+        {
+            ViewBag.Message = "Oyuncular";
+
+            return View();
+        }
+
+        public ActionResult CompetitionList()
+        {
+            ViewBag.Message = "Müsabakalar";
+
+            return View();
+        }
+
+        public ActionResult RefereeList()
+        {
+            ViewBag.Message = "Hakemler";
+
+            return View();
+        }
+
+        public ActionResult Profil()
+        {
+            ViewBag.Message = "Profil";
+
+            return View();
+        }
+    }
+}
