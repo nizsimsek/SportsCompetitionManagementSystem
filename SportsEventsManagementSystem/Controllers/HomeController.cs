b830d62fba5f9f7c8db9fdb73853281451c528f7
nizsimsek <19720111036@ybu.edu.tr>
2021-06-26 01:39:45 +0300

    Update Forms Added.

--- SportsEventsManagementSystem/Controllers/HomeController.cs
+++ SportsEventsManagementSystem/Controllers/HomeController.cs
@@ -8,51 +8,135 @@ namespace SportsEventsManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Login()
         {
-            ViewBag.Message = "Giriş Yap";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username.Equals("acc1") && password.Equals("123"))
+            {
+                Session["Username"] = username.ToString().ToUpper();
+                ViewBag.Message = "Anasayfa";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.error = "Yanlış kullanıcı adı veya şifre..";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Remove("Username");
+            return RedirectToAction("Login", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Kayıt Ol";
 
             return View();
         }
 
-        public ActionResult LogOut()
+        [HttpPost]
+        public ActionResult Register(string username, string email, string password, string name, string surname)
         {
-            Session.RemoveAll();
-            return View(Login());
+            // TODO: Kayıt işlemleri yapılacak
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UpdatePlayer()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UpdateReferee()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCompetition()
+        {
+            return View();
         }
 
         public ActionResult Index()
         {
-            ViewBag.Message = "Anasayfa";
+            if (Session["Username"] != null)
+            {
+                ViewBag.Message = "Anasayfa";
 
-            return View();
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
         }
         public ActionResult PlayerList()
         {
-            ViewBag.Message = "Oyuncular";
+            if (Session["Username"] != null)
+            {
+                ViewBag.Message = "Oyuncular";
 
-            return View();
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
         }
 
         public ActionResult CompetitionList()
         {
-            ViewBag.Message = "Müsabakalar";
+            if (Session["Username"] != null)
+            {
+                ViewBag.Message = "Müsabakalar";
 
-            return View();
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
         }
 
         public ActionResult RefereeList()
         {
-            ViewBag.Message = "Hakemler";
+            if (Session["Username"] != null)
+            {
+                ViewBag.Message = "Hakemler";
 
-            return View();
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
         }
 
         public ActionResult Profil()
         {
-            ViewBag.Message = "Profil";
+            if (Session["Username"] != null)
+            {
+                ViewBag.Message = "Profil";
 
-            return View();
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
         }
     }
 }
